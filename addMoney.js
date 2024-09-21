00a967f91ed5a5de68711a4b8c0e87da7e33206a
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-09-21 10:22:59 +0600

    Transactions System

--- addMoney.js
+++ addMoney.js
@@ -4,10 +4,17 @@ document
     event.preventDefault();
     const addMoney = getInputFieldValueById("input-add-money");
     const pinNumber = getInputFieldValueById("input-pin-number");
+    if (isNaN(addMoney)) {
+      return
+    }
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById("account-balance");
       const newBalance = balance + addMoney;
       document.getElementById("account-balance").innerText = newBalance;
+      // Add to Transactio History
+      const p = document.createElement("p");
+      p.innerText = `Cash In: ${addMoney} Tk. Balance: ${newBalance} Tk.`;
+      document.getElementById("transactions-container").appendChild(p);
     } else {
       alert("Failed to add money!");
     }
--- cashOut.js
+++ cashOut.js
@@ -4,10 +4,25 @@ document
     event.preventDefault();
     const cashOut = getInputFieldValueById("cash-out-amount");
     const pinNumber = getInputFieldValueById("cash-out-pin");
+    if (isNaN(cashOut)) {
+      return;
+    }
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById("account-balance");
+      if (cashOut > balance) {
+        alert("You do not have enough money!");
+        return;
+      }
       const newBalance = balance - cashOut;
       document.getElementById("account-balance").innerText = newBalance;
+      // Add to Transactio History
+      const div = document.createElement("div");
+      div.classList.add("bg-yellow-300");
+      div.innerHTML = `
+      <h4 class="text-2xl font-bold">Cash Out</h4>
+      <p>${cashOut} Tk withdraw. Balance ${newBalance} Tk.</p>
+      `;
+      document.getElementById("transactions-container").appendChild(div);
     } else {
       alert("No money for you...DGM!");
     }
